f5af4797cfd9e977888b28424c0098e061db5df3
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-02-26 21:00:39 +0800

    add `/questions` route
    - return a JSON response of the `Question` struct
    - add the Question struct from the previous chapters

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,66 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
+use axum::Json;
 use axum::{routing::get, Router};
+
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", get(index));
+    let app = Router::new().route("/questions", get(get_questions));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!(
+        "->> LISTENING on: {:?}\n",
+        listener
+            .local_addr()
+            .expect("Failed to get local address of the listener")
+    );
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn index() -> &'static str {
-    "Hello, World!"
+async fn get_questions() -> Json<Question> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of the question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    Json(question)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct QuestionId(String);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Self {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No ID provided")),
+        }
+    }
 }
